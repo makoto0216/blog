bc3e45c642e150d6c35d50fcf4e60f73e66b8ec9
makoto0216 <b203634@osaka-ue.ac.jp>
2023-01-06 21:23:52 +0900

    エラーメッセージ日本語化

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,23 +10,26 @@
       
     </head>
     <body class ="antialiased">
-       <h1>Blog Name</h1>
+       <h1 class="title">編集画面</h1>
+       <div class=content>
        <form action="/posts/{{ $post->id }}" method="POST">
            @csrf
            @method('PUT')
-           <div class= "title">
+           <div class= "content_title">
+               <h2>タイトル</h2>
                <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
-               <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+               <p class="content_title__error" style="color:red">{{ $errors->first('post.title') }}</p>
            </div>
-           <div class= "body">
-               <h2>Body</h2>
+           <div class= "content_body">
+               <h2>本文</h2>
                <textarea name= "post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
-               <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+               <p class="content_body__error" style="color:red">{{ $errors->first('post.body') }}</p>
            </div>
-           <input type="submit" value="update">
+           <input type="submit" value="更新">
         </from>
         <div class = 'footer'>
            <a href="/posts/{{ $post->id }}">戻る</a>
+    　 </div>
     　 </div>
     </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,7 +19,7 @@
                </div>
        </div>
        <div class = 'edit'>
-           <a href="/posts/{{ $post->id }}/edit">edit</a>
+           <a href="/posts/{{ $post->id }}/edit">編集</a>
        </div>
        <div class = 'footer'>
            <a href="/">戻る</a>
